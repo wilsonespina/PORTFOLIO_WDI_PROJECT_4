babc8738afe0f254f354d4f0f1300487b75fad50
wilsonespina <wilson.espina@gmail.com>
2017-12-16 06:17:32 +0000

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     open: true,
     proxy: {
       '/api': {
-        target: 'https://protected-spire-24454.herokuapp.com/',
+        target: 'https://protected-spire-24454.herokuapp.com',
         secure: false
       }
     }
