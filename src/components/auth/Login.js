22e81bcdf86522ea9b03162331deaa68a402c7f7
wilsonespina <wilson.espina@gmail.com>
2017-12-20 11:23:47 +0000

    pushing to heroku

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import OAuthButton from './OAuthButton';
 
-// import css from '../../scss/components/login.scss';
+// import loginBackground from '../../src/assets/icons/footpath-logo.png';
 
 class Login extends React.Component {
-
   render() {
+    const iconStyle = {
+      background: 'url(/../../src/assets/Strava_Logo.jpg)',
+      height: '20px',
+      width: '20px',
+      display: 'inline-block',
+      'background-size': 'cover'
+    };
+
+
     return (
       <div className="login-all">
 
@@ -15,8 +23,8 @@ class Login extends React.Component {
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 login-right">
 
           <div className="button-wrapper">
-            <h1 className="nav-trace-login">TRACE <img src="../../src/assets/icons/footpath-logo.jpg" className="foot-icon-login" /></h1>
-            <OAuthButton provider="strava"><p><span className="icon"></span> Login with Strava</p></OAuthButton>
+            <h1 className="nav-trace-login">TRACE <img src='../../src/assets/icons/footpath-logo.png' className="foot-icon-login" /></h1>
+            <OAuthButton provider="strava"><p><span className="icon" style={iconStyle}></span> Login with Strava</p></OAuthButton>
             <br/>
             <a
               className="btn btn-primary"
